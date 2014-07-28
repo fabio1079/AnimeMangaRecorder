314d635a73bb3435ee24de6dc04f3813fdf54027
Fabio Teixeira <fabio1079@gmail.com>
2014-07-28 17:17:22 -0300

    Adding fiendly_id to tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,8 @@
 class Tag < ActiveRecord::Base
+  extend FriendlyId
+
   has_and_belongs_to_many :animes
   has_and_belongs_to_many :mangas
+
+  friendly_id :name, use: :slugged
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140728200525) do
+ActiveRecord::Schema.define(version: 20140728200800) do
 
   create_table "animes", force: true do |t|
     t.string   "title"
@@ -76,6 +76,9 @@ ActiveRecord::Schema.define(version: 20140728200525) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "slug"
   end
+
+  add_index "tags", ["slug"], name: "index_tags_on_slug", unique: true
 
 end
