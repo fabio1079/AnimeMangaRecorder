0fd2fbc2c8d12c35abcda093d6028cb8021279ca
Fabio Teixeira <fabio1079@gmail.com>
2014-07-28 17:36:43 -0300

    Adding validations to Manga

--- app/models/manga.rb
+++ app/models/manga.rb
@@ -5,4 +5,17 @@ class Manga < ActiveRecord::Base
   has_and_belongs_to_many :tags
 
   friendly_id :title, use: :slugged
+
+  validates :title,
+    :presence=>true,
+    :uniqueness=>true,
+    :length=>{ :minimum=>3, :maximum=>80 }
+
+  validates :volume, :chapter,
+    :presence=>true,
+    :length=>{ :minimum=>1, :maximum=>10 }
+
+  validates :chapter_title,
+    :length=>{ :maximum=>80 },
+    :allow_blank=>true
 end
