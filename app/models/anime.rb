25b6f6c03b64da4d7732fe99b7d0613375034175
Fabio Teixeira <fabio1079@gmail.com>
2014-07-28 17:29:35 -0300

    Adding validations to Anime

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -5,4 +5,16 @@ class Anime < ActiveRecord::Base
   has_and_belongs_to_many :tags
 
   friendly_id :title, use: :slugged
+
+  validates :title,
+    :presence=>true,
+    :uniqueness=>true,
+    :length=>{ :minimum=>2, :maximum=>80 }
+
+  validates :season_title,
+    :length=>{ :maximum=>80 }
+
+  validates :season, :episode,
+    :presence=>true,
+    :length=>{ :minimum=>1, :maximum=>10 }
 end
