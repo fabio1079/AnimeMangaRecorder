60fd7660f6329abb538071520fb35965e2e96610
Fabio Teixeira <fabio1079@gmail.com>
2014-07-28 16:15:22 -0300

    scaffolding tag

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140728191318) do
+ActiveRecord::Schema.define(version: 20140728191438) do
 
   create_table "animes", force: true do |t|
     t.string   "title"
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 20140728191318) do
     t.string   "volume"
     t.integer  "chapter"
     t.string   "chapter_title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
