5f2cb4d8ede35acf10a9efc685388e402786253b
Fabio Teixeira <fabio1079@gmail.com>
2014-07-28 17:31:35 -0300

    Adding validation to Author

--- app/models/author.rb
+++ app/models/author.rb
@@ -5,4 +5,9 @@ class Author < ActiveRecord::Base
   has_many :mangas
 
   friendly_id :name, use: :slugged
+
+  validates :name,
+    :presence=>true,
+    :uniqueness=>true,
+    :length=>{ :minimum=>2, :maximum=>80 }
 end
