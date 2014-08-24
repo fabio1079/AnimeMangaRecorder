47c2f370e91ac32a819431c397a0e6154e39fda8
Fabio Teixeira <fabio1079@gmail.com>
2014-08-24 14:24:00 -0300

    Adjusting main menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def menu_link text, link
+    if params[:controller] == link
+      link_to text, "/#{link}", :class=>"btn active"
+    else
+      link_to text, "/#{link}", :class=>"btn"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root :to=> "home#index"
-  get 'home/index'
+  get "/home", :to=>"home#index"
 
   resources :tags
 
